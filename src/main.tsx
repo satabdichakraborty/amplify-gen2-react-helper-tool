54af970f2c27bd54ac890fdf9c5b5da8e5f0335c
satabdi.chakraborty <satabdi.d@gmail.com>
2025-03-16 11:59:30 -0400

    fix: move amplify outputs files to root directory

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Amplify } from "aws-amplify";
 import { generateClient } from "@aws-amplify/api";
 import { type Schema } from '../amplify/data/resource';
-import outputs from "./amplify_outputs.json";
+import outputs from "../amplify_outputs.json";
 import App from "./App";
 import "./index.css";
 
